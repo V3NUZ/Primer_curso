41418f27ca681e4d4d729557181aafdaf3465801
Alejandro Cardona <yeisoonc@gmail.com>
2024-04-23 15:41:50 -0500

    Funciones

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -2,3 +2,15 @@ def pedir_pizza():
     print("¡Quiero pizza!")
 
 pedir_pizza()
+
+def checar_entrada(edad):
+    if edad >= 18:
+        print("Eres mayor de edad")
+    else:
+        print("Eres menor de edad")
+
+edad = 22
+edad_2 = 17
+
+checar_entrada(edad)
+checar_entrada(edad_2)
